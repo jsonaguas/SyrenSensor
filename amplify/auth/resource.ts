7d28070f039d696c9eef4122b3662cc7c6dfcc89
Matthew Gladkowski <matthew.gladkowski@gmail.com>
2025-04-15 12:48:20 -0400

    auth structure update

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,29 +1,17 @@
 import { referenceAuth } from "@aws-amplify/backend";
 
+const getEnvVar = (name: string): string => {
+	const value = process.env[name]?.toString();
+	if (!value) {
+		throw new Error(`${name} is not defined in environment variables`);
+	}
+	return value;
+};
+
 export const auth = referenceAuth({
-	userPoolId:
-		process.env.USER_POOL_ID?.toString() ??
-		(() => {
-			throw new Error("USER_POOL_ID is not defined");
-		})(),
-	identityPoolId:
-		process.env.IDENTITY_POOL_ID?.toString() ??
-		(() => {
-			throw new Error("IDENTITY_POOL_ID is not defined");
-		})(),
-	authRoleArn:
-		process.env.AUTH_ROLE_ARN?.toString() ??
-		(() => {
-			throw new Error("AUTH_ROLE_ARN is not defined");
-		})(),
-	unauthRoleArn:
-		process.env.UNAUTH_ROLE_ARN?.toString() ??
-		(() => {
-			throw new Error("UNAUTH_ROLE_ARN is not defined");
-		})(),
-	userPoolClientId:
-		process.env.USER_POOL_CLIENT_ID?.toString() ??
-		(() => {
-			throw new Error("USER_POOL_CLIENT_ID is not defined");
-		})(),
+	userPoolId: getEnvVar("USER_POOL_ID"),
+	identityPoolId: getEnvVar("IDENTITY_POOL_ID"),
+	authRoleArn: getEnvVar("AUTH_ROLE_ARN"),
+	unauthRoleArn: getEnvVar("UNAUTH_ROLE_ARN"),
+	userPoolClientId: getEnvVar("USER_POOL_CLIENT_ID"),
 });
