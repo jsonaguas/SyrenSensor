2e242b98c2c0e9a1292d23ea228b66aacdb173dd
jsonaguas <jaguas43@gmail.com>
2025-04-16 20:16:41 -0500

    updated client id

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -2,7 +2,7 @@ const awsmobile = {
     aws_project_region: 'us-east-1',
     aws_cognito_region: 'us-east-1',
     aws_user_pools_id: 'us-east-1_WAM9G0wpt',
-    aws_user_pools_web_client_id: 'd113xysrintyrk',
+    aws_user_pools_web_client_id: '4g82g3an13rlgcdrdsb09k32u3',
     oauth: {}, // optional unless you're using OAuth/social sign-in
   };
   
