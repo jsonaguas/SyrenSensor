7d58baf9df35dca11581223f24b6d1f0b65a0250
Matthew Gladkowski <matthew.gladkowski@gmail.com>
2025-04-20 13:09:57 -0400

    attribute update

--- src/main.tsx
+++ src/main.tsx
@@ -9,9 +9,40 @@ import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
+const formFields = {
+	signUp: {
+		email: {
+			order: 1,
+		},
+		password: {
+			order: 2,
+		},
+		confirm_password: {
+			order: 3,
+		},
+		gender: {
+			order: 4,
+			label: "Gender",
+			placeholder: "Enter your Gender",
+		},
+		name: {
+			order: 5,
+		},
+		address: {
+			order: 6,
+		},
+		birthdate: {
+			order: 7,
+		},
+		phone_number: {
+			order: 8,
+		},
+	},
+};
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<Authenticator signUpAttributes={["gender", "address"]}>
+		<Authenticator>
 			<App />
 		</Authenticator>
 	</React.StrictMode>
