df3c9f23ad867774d1885f3900fb5883b27c36e7
jsonaguas <jaguas43@gmail.com>
2025-05-14 00:58:36 -0500

    resource

--- amplify/functions/seedWearableData/resource.ts
+++ amplify/functions/seedWearableData/resource.ts
@@ -1,10 +1,9 @@
 import { defineFunction } from "@aws-amplify/backend";
-import { data } from "../../data/resource";
 
 export const seedWearableData = defineFunction({
   name: "seedWearableData",
   entry: "./index.ts",
   environment: {
-    NEW_TABLE_NAME: data.WearableRawData.name,
+    NEW_TABLE_NAME: process.env.DATA_WEARABLERAWDATA_NAME!,
   },
 });
