5d456ce6d2b588141be918944b27441867cccb84
jsonaguas <jaguas43@gmail.com>
2025-05-13 23:54:55 -0500

    added back seed

--- amplify/functions/seedWearableData/resource.ts
+++ amplify/functions/seedWearableData/resource.ts
@@ -2,7 +2,7 @@ import { defineFunction } from "@aws-amplify/backend";
 
 export const seedWearableData = defineFunction({
   name: "seedWearableData",
-  entry: "./index.ts",
+  entry: "./amplify/functions/seedWearableData/index.ts",
   environment: {
     RAW_TABLE_NAME: process.env.DATA_WEARABLERAWDATA_NAME!,
   },
